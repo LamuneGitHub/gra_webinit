65f83e06a04928f7765abb839d0a94f7232d702c
이상필 <nb90487@navercorp.com>
2017-06-12 17:43:11 +0900

    최초 git init

---
+++ src/main/java/com/lamune/sb154/gra_webinit/gra_webinit/GraWebinitApplication.java
@@ -0,0 +1,12 @@
+package com.lamune.sb154.gra_webinit.gra_webinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraWebinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraWebinitApplication.class, args);
+	}
+}
